0a3b162ebd9d6e58313652ed65b0a177c4ba44a1
Lakota Larson <lakota-larson@uiowa.edu>
2019-10-10 21:22:04 -0500

    modified application to use basic Jinja templates

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html', pageTitle='Loan Calculator')
 
 @app.route('/lakota')
 def lakota():
-    return "hello lakota!"
+    return render_template('lakota.html', pageTitle='About Lakota')
 
 if __name__ == '__main__':
     app.run(debug=True)
